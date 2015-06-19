7196e5c556702cb0abf649c112797f4fe692f036
Johannes Kulick <kulick@hildensia.de>
2015-06-19 20:57:54 +0200

    auto resize iframes

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,5 @@
 //custom js goes here
+function resizeIframe(obj) {
+  obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+}
 //
-// Comments
-//var thispage = document.location.href;
-
-//$.getJSON('http://pooleapp.com/data/da746a61-267c-4c40-9671-d0c2eb343bb5.json', function(d) {
-  //$.each(d, function(key, entries) {
-    //$.each(entries, function(idx, entry) {
-      //if ('test-id-253789' == entry['id']) {
-        //console.log(entry)
-        //var paragraph = document.createElement("p");
-        //var name = document.createTextNode("Name: " + entry['name']);
-        //var date = document.createTextNode("Date: " + entry['created']);
-        //var website = document.createTextNode("Website: " + entry['url']);
-        //var comment = document.createTextNode(entry['comment']);
-        //paragraph.appendChild(name);
-        //paragraph.appendChild(date);
-        //paragraph.appendChild(website);
-        //paragraph.appendChild(comment);
-        //document.body.appendChild(paragraph);
-      //}
-    //});
-  //});
-//});
